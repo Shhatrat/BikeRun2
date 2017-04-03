905a0d0bf57268987e4816e8d05e90deb4b1dcbb
Szymon Kaminski <szymonkaminski.ptb@gmail.com>
2017-04-03 12:50:16 +0000

    add butterknife

--- app/src/main/java/com/shhatrat/bikerun2/view/activity/MenuActivityView.java
+++ app/src/main/java/com/shhatrat/bikerun2/view/activity/MenuActivityView.java
@@ -3,24 +3,42 @@ package com.shhatrat.bikerun2.view.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
+import com.lid.lib.LabelImageView;
 import com.shhatrat.bikerun2.R;
 import com.shhatrat.bikerun2.presenter.activity.IMenuActivityPresenter;
 import com.shhatrat.bikerun2.presenter.activity.MenuActivityPresenter;
 
-public class MenuActivityView extends AppCompatActivity implements IMenuActivityView{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
+public class MenuActivityView extends AppCompatActivity implements IMenuActivityView {
+
+    @BindView(R.id.menu_imagebutton_running)
+    ImageButton menuImagebuttonRunning;
+    @BindView(R.id.menu_imagebutton_bike)
+    ImageButton menuImagebuttonBike;
+    @BindView(R.id.menu_labelimagebutton_strava)
+    LabelImageView menuLabelimagebuttonStrava;
+    @BindView(R.id.menu_imagebutton_strava)
+    ImageButton menuImagebuttonStrava;
+    @BindView(R.id.cm_imagebutton_trainer)
+    ImageButton cmImagebuttonTrainer;
     private IMenuActivityPresenter menuActivityPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        menuActivityPresenter = new MenuActivityPresenter(this,this);
+        menuActivityPresenter = new MenuActivityPresenter(this, this);
     }
 
     @Override
@@ -33,7 +51,7 @@ public class MenuActivityView extends AppCompatActivity implements IMenuActivity
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if(id == R.id.action_settings) {
+        if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
